ea0c2ed0a39a69c7fea4d1965094ef0a8a2d2383
Md Israfil <sikdermdisrafil4@gmail.com>
2024-06-06 22:32:51 +0600

    all done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Battles from './components/Bottles/Bottles'
+import Header from './components/Header/Header'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header></Header>
+      <Battles></Battles>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Bottle/Bottle.jsx
@@ -0,0 +1,22 @@
+import propTypes from 'prop-types';
+import './bottle.css';
+const Bottle = ({bottle,handleAddToCart}) => {
+
+    const {name,img,price}=bottle;
+
+    //console.log(bottle)
+    return (
+        <div className="bottle">
+            <h3>bottle:{name}</h3>
+            <img src={img} alt="" />
+            <p>Price:${price}</p>
+
+            <button onClick={()=>handleAddToCart(bottle)}>Purchase</button>
+        </div>
+    );
+};Bottle.propTypes ={
+    bottle:propTypes.object.isRequired,
+    handleAddToCart:propTypes.func.isRequired
+}
+
+export default Bottle;
